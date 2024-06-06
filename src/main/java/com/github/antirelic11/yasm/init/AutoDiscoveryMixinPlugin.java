d7b3b9e40ebdb905a0b7587c27ed285574c1add7
GitHub Action <action@github.com>
2024-06-06 21:38:27 +0000

    Initialize template repository proper
    Set package name to com.github.antirelic11.yasm
    Set project name to YASM
    Set modid to yasm

--- src/main/java/com/github/antirelic11/yasm/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/antirelic11/yasm/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.antirelic11.yasm.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
